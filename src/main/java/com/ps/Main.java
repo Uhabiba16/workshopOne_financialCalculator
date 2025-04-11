838f900cfdbbf429b88af84f3fffbc0dd1274608
Umme Habiba <humme.career@gmail.com>
2025-04-11 17:40:57 -0400

    done mortgage calculator

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -5,36 +5,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
 
-//        System.out.println("Principal");
-//        System.out.println("Interest Rate");
-//        System.out.println("Loan Length");
-//        System.out.println("Please provide input data:");
+//        INPUTS: Principal,Interest Rate, Loan Length
 
-
-        System.out.println("input principal:$");
+        System.out.print("input principal:$ ");
         double principal = scanner.nextDouble();
 
-        System.out.println("input annual interest rate:%");
+        System.out.print("input annual interest rate:% ");
         double interestRate = scanner.nextDouble();
 
-        System.out.println("input loan length: Years");
+        System.out.print("input loan length in Years: ");
         int loanLength = scanner.nextInt();
 
         int totalMonths =0;
         totalMonths = loanLength * 12;
-        System.out.println(totalMonths);
+        System.out.println("Number of monthly payments: "+ totalMonths);
 
+        double annualInterest= 0;
+        annualInterest= (principal*(interestRate/100));
+        System.out.println("Total annual interest:$ "+ annualInterest);
 
+        double monthlyInterest=0;
+        monthlyInterest= (principal+ annualInterest)/totalMonths;
 
+        System.out.printf("Monthly payment:$%.2f", monthlyInterest);
 
-
-
-
-
-
-
-
-//Calculator 1
+// -------------------------------------END OF MORTGAGE CALCULATOR--------------------------------------
 
 
 
