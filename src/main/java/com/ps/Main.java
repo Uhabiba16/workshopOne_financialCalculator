2ae8a1f87826681985b294e89d18a00c66bb59e8
Umme Habiba <humme.career@gmail.com>
2025-04-11 15:58:29 -0400

    inputs

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,36 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+//        System.out.println("Principal");
+//        System.out.println("Interest Rate");
+//        System.out.println("Loan Length");
+//        System.out.println("Please provide input data:");
+
+
+        System.out.println("input principal:$");
+        double principal = scanner.nextDouble();
+
+        System.out.println("input annual interest rate:%");
+        double interestRate = scanner.nextDouble();
+
+        System.out.println("input loan length: Years");
+        int loanLength = scanner.nextInt();
+
+        double totalInterest=0;
+
+
+
+
+
+
+
+//Calculator 1
+
+
 
     }
 }
