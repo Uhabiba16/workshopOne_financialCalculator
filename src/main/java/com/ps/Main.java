eedd5a9b84fc9ac93d417d41d346c00d887db6d5
Umme Habiba <humme.career@gmail.com>
2025-04-11 23:04:04 -0400

    work inprogress

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -6,40 +6,54 @@ public class Main {
         Scanner scanner= new Scanner(System.in);
 
 //        INPUTS: Principal,Interest Rate, Loan Length
-
-        System.out.print("input principal:$ ");
-        double principal = scanner.nextDouble();
-
-        System.out.print("input annual interest rate: ");
-        double interestRate = scanner.nextDouble();
-
-        System.out.print("input loan length in Years: ");
-        int loanLength = scanner.nextInt();
-
-        int totalMonths =0;
-        totalMonths = (loanLength * 12);
-        System.out.println("Number of monthly payments: "+ totalMonths);
-
-        double annualInterest= 0;
-        annualInterest= (principal*(interestRate/100));
-        System.out.println("Total annual interest:$ "+ annualInterest);
-
-        double monthlyInterest=0;
-        monthlyInterest= ((principal+ annualInterest)/totalMonths);
-
-        System.out.printf("Monthly payment:$%.2f", monthlyInterest);
+//
+//        System.out.print("input principal:$ ");
+//        double principal = scanner.nextDouble();
+//
+//        System.out.print("input annual interest rate: ");
+//        double interestRate = scanner.nextDouble();
+//
+//        System.out.print("input loan length in Years: ");
+//        int loanLength = scanner.nextInt();
+//
+//        int totalMonths =0;
+//        totalMonths = (loanLength * 12);
+//        System.out.println("Number of monthly payments: "+ totalMonths);
+//
+//        double annualInterest= 0;
+//        annualInterest= (principal*(interestRate/100));
+//        System.out.println("Total annual interest:$ "+ annualInterest);
+//
+//        double monthlyInterest=0;
+//        monthlyInterest= ((principal+ annualInterest)/totalMonths);
+//
+//        System.out.printf("Monthly payment:$%.2f", monthlyInterest);
 
 // -------------------------------------END OF MORTGAGE CALCULATOR--------------------------------------
 
 //        -----------CALCULATOR 2------------FIND THE FUTURE VALUE-----------------------
-        System.out.println("Input Deposit: $ ");
+        System.out.print("Input Deposit: $ ");
         int deposit = scanner.nextInt();
 
-        System.out.println("Input interest:% ");
+        System.out.print("Input interest:% ");
         double interest= scanner.nextDouble();
 
-        System.out.println("Input years: ");
-        int days = scanner.nextInt();
+        double rate=0;
+        rate =(interest /100);
+
+        System.out.print("Input years: ");
+        int years = scanner.nextInt();
+
+        int days =0;
+        days = (years * 365);
+
+        float futureValue=0;
+
+
+//        double futureValue=0;
+//        futureValue= (deposit* ((1+ interest/365)* (days)));
+
+        System.out.printf("future value:$%.2f", futureValue);
 
     }
 }
